89fa0b937ffb63b88eaab22217091a57aa82b2f0
Unknown <bengordon1995@gmail.com>
2019-03-30 20:04:40 -0700

    Added database constructor script, and basic home page

---
+++ Controller.php
@@ -0,0 +1,60 @@
+<?php
+
+session_start();
+require_once "DatabaseAdaptor.php";
+$databaseAdaptor = new DatabaseAdaptor();
+
+
+if (isset($_POST["method"])){
+    $method = $_POST["method"];
+}
+
+//user login method
+
+//manages user login attemps
+if ($method == "login"){
+    if (isset($_POST["username"])){
+        $username = $_POST["username"];
+    }
+    if (isset($_POST["password"])){
+        $password = $_POST["password"];
+    }
+    if (isset($_POST["username"]) && isset($_POST["password"])){
+        if ($databaseAdaptor->checkUserCredentials($username, $password) == TRUE){
+            $_SESSION["user"] = $username;
+            echo true;
+        }
+        else{
+            echo false;
+        }
+    }
+}
+
+//manages user registrations
+if ($method == "registration"){
+    if (isset($_POST["username"])){
+        $username = $_POST["username"];
+    }
+    if (isset($_POST["password"])){
+        $password = $_POST["password"];
+    }
+    
+    $arr = $databaseAdaptor->checkForUsername($username);
+    if (count($arr) > 0){
+        echo "Userame taken, try a different username";
+    }
+    else{
+        $databaseAdaptor->insertNewUser($username, $password);
+        echo 'Registration succesful';
+    }
+}
+
+
+
+
+
+
+
+
+?>
+
